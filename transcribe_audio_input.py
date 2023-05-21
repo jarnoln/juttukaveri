25b9f8b7876e758cc3aa83c62ee4b10d8a4d3142
Jarno Luoma-Nirva <jln@iki.fi>
2023-05-21 19:27:54 +0300

    Use pyttsx3 for voice output

--- transcribe_audio_input.py
+++ transcribe_audio_input.py
@@ -1,14 +1,34 @@
 import argparse
+import pyttsx3
 import speech_recognition
+import time
+
+
+def print_tts_info(tts):
+    voices = tts.getProperty('voices')
+    for voice in voices:
+        print(voice)
+    rate = tts.getProperty('rate')
+    print('Rate:')
+    print(rate)
 
 
 def transcribe_audio_input():
     recognizer = speech_recognition.Recognizer()
     mic = speech_recognition.Microphone()
+    tts = pyttsx3.init()
+    # print_tts_info(tts)
+    tts.setProperty('voice', 'finnish')
+    tts.setProperty('rate', 120)
     with mic as source:
         recognizer.adjust_for_ambient_noise(source)
+        greet = 'Sano jotain'
+        print(greet)
+        tts.say(greet)
+        tts.runAndWait()
+        time.sleep(0.5)
         while True:
-            print('Sano jotain')
+            print('Kuuntelen')
             audio = recognizer.listen(source)
             try:
                 result_google = recognizer.recognize_google(audio, language='fi-FI')
@@ -21,6 +41,8 @@ def transcribe_audio_input():
                 continue
             print('Google:')
             print(result_google)
+            tts.say(result_google)
+            tts.runAndWait()
             if 'heippa' in result_google.lower():
                 break
 
