0bfec63ac7ac514db45a9f02122a82d5cb4f10e6
Jarno Luoma-Nirva <jln@iki.fi>
2023-06-11 11:34:02 +0300

    Send inputs to ChatGPT and say responses

--- transcribe_audio_input.py
+++ transcribe_audio_input.py
@@ -1,7 +1,12 @@
 import argparse
+import openai
+import os
 import pyttsx3
 import speech_recognition
 import time
+
+
+OPENAI_KEY = os.environ.get('OPENAI_KEY', '')
 
 
 def print_tts_info(tts):
@@ -13,6 +18,19 @@ def print_tts_info(tts):
     print(rate)
 
 
+def create_response_text(messages):
+    print('Sending messages:')
+    print(messages)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    print(response)
+    response_text = response.choices[0]['message']['content']
+    print(response_text)
+    return response_text
+
+
 def transcribe_audio_input():
     recognizer = speech_recognition.Recognizer()
     mic = speech_recognition.Microphone()
@@ -20,10 +38,14 @@ def transcribe_audio_input():
     # print_tts_info(tts)
     tts.setProperty('voice', 'finnish')
     tts.setProperty('rate', 120)
-    greet = 'Sano jotain'
+    greet = 'Hei! Kuka sinä olet?'
     print(greet)
     tts.say(greet)
     tts.runAndWait()
+    messages = [
+        {"role": "system", "content": "Olet ystävällinen lastenopettaja. Keskustelet 4-vuotiaan lapsen kanssa."},
+        {"role": "assistant", "content": greet}
+    ]
     while True:
         with mic as source:
             print('Default energy threshold: {}'.format(recognizer.energy_threshold))
@@ -44,14 +66,22 @@ def transcribe_audio_input():
                 print('Sori, en saanut selvää')
                 continue
             print(result_google)
-            tts.say(result_google)
-            tts.runAndWait()
+            if result_google.strip():
+                if 'heippa' or 'riittää' in result_google.lower():
+                    break
+                messages.append({"role": "user", "content": result_google})
+                response = create_response_text(messages)
+                messages.append({"role": "assistant", "content": response})
+                tts.say(response)
+                tts.runAndWait()
             # time.sleep(2)
-            if 'heippa' in result_google.lower():
-                break
 
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
     # args = parser.parse_args()
+    if OPENAI_KEY:
+        openai.api_key = OPENAI_KEY
+    else:
+        print('Need to set you openai key: export OPENAI_KEY={your key}')
     transcribe_audio_input()
