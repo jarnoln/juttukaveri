b4ce070d1ada1de93eea6ab9cb3b12f8d3aed6a8
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-02 23:27:36 +0300

    Script testing AWS Polly TTS

---
+++ scripts/test_polly.py
@@ -0,0 +1,32 @@
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import sys
+
+session = Session(profile_name="default")
+polly = session.client("polly")
+
+try:
+    # Request speech synthesis
+    response = polly.synthesize_speech(
+        Text="Hei! Kuka sinä olet?",
+        LanguageCode="fi-FI",
+        OutputFormat="mp3",
+        VoiceId="Suvi",
+        Engine="neural"
+    )
+    print(response)
+except (BotoCoreError, ClientError) as error:
+    # The service returned an error, exit gracefully
+    print(error)
+    sys.exit(-1)
+
+if "AudioStream" in response:
+    with closing(response["AudioStream"]) as stream:
+        out_file_name = "polly.mp3"
+        try:
+            with open(out_file_name, "wb") as out_file:
+                out_file.write(stream.read())
+        except IOError as error:
+            print(error)
+            sys.exit(-1)
