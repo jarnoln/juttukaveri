2c4a4b8db35e2ac10ddb6b081ad1164631ac2965
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-05 23:30:04 +0300

    Separate index page

--- view/urls.py
+++ view/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("j/", views.juttukaveri, name="juttukaveri"),
     path("", views.index, name="index"),
 ]
--- view/views.py
+++ view/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(template_name='view/index.html', request=None)
+
+
+def juttukaveri(request):
     return render(template_name='view/juttukaveri.html', request=None)
