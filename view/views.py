6ee4bb43cbf23460e341c36c0fd46ccc0e7460db
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-05 23:22:16 +0300

    Remove old template and rename index.html

--- view/views.py
+++ view/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(template_name='view/index.html', request=None)
+    return render(template_name='view/juttukaveri.html', request=None)
