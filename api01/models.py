25cedafdd58bf9219b724a7ef9494cdbcba389c9
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-20 19:59:25 +0300

    Add Transcript and Reply models

--- api01/models.py
+++ api01/models.py
@@ -9,3 +9,23 @@ class Session(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(str(self.session_id), str(self.created))
+
+
+class Transcript(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='transcripts')
+    audio_url = models.CharField(max_length=500)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}:{}'.format(str(self.session.session_id), str(self.created))
+
+
+class Reply(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='replies')
+    audio_url = models.CharField(max_length=500)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}:{}'.format(str(self.session.session_id), str(self.created))
--- api01/tests/test_models.py
+++ api01/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from api01.models import Session
+from api01.models import Session, Transcript, Reply
 
 
 class SessionModelTests(TestCase):
@@ -13,3 +13,33 @@ class SessionModelTests(TestCase):
         session = Session.objects.create(session_id='abc')
         session_str = str(session)
         self.assertTrue(session_str.startswith('abc:'))
+
+
+class TranscriptModelTests(TestCase):
+    def test_can_save_and_load(self):
+        session = Session.objects.create(session_id='abc')
+        transcript = Transcript(session=session)
+        transcript.save()
+        self.assertEqual(Transcript.objects.count(), 1)
+        self.assertEqual(Transcript.objects.first(), transcript)
+
+    def test_string(self):
+        session = Session.objects.create(session_id='abc')
+        transcript = Transcript.objects.create(session=session)
+        transcript_str = str(transcript)
+        self.assertTrue(transcript_str.startswith('abc:'))
+
+
+class ReplyModelTests(TestCase):
+    def test_can_save_and_load(self):
+        session = Session.objects.create(session_id='abc')
+        reply = Reply(session=session)
+        reply.save()
+        self.assertEqual(Reply.objects.count(), 1)
+        self.assertEqual(Reply.objects.first(), reply)
+
+    def test_string(self):
+        session = Session.objects.create(session_id='abc')
+        reply = Reply.objects.create(session=session)
+        reply_str = str(reply)
+        self.assertTrue(reply_str.startswith('abc:'))
