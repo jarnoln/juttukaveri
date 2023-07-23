a5f6348e908ff1563201c4505365dbe87aac1dba
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-23 18:27:49 +0300

    Make extra session fields optional

--- api01/models.py
+++ api01/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Session(models.Model):
     session_id = models.CharField(max_length=50)
-    ip = models.CharField(max_length=20, default="")
-    agent = models.CharField(max_length=500, default="")
-    referer = models.CharField(max_length=500, default="")
+    ip = models.CharField(max_length=20, default="", null=True, blank=True)
+    agent = models.CharField(max_length=500, default="", null=True, blank=True)
+    referer = models.CharField(max_length=500, default="", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
