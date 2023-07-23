7af875d951e7b3a4dae56399f5c515ebaf2096fe
Jarno Luoma-Nirva <jln@iki.fi>
2023-07-23 18:25:59 +0300

    Add page for listing sessions

--- view/urls.py
+++ view/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("sessiot/", views.sessions, name="sessions"),
     path("j/", views.juttukaveri, name="juttukaveri"),
     path("", views.index, name="index"),
 ]
--- view/views.py
+++ view/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from api01.models import Session
 
 
 # Create your views here.
@@ -8,3 +9,10 @@ def index(request):
 
 def juttukaveri(request):
     return render(template_name="view/juttukaveri.html", request=None)
+
+
+def sessions(request):
+    context = {
+        'sessions': Session.objects.all().order_by('created')
+    }
+    return render(request=request, template_name="view/sessions.html", context=context)
