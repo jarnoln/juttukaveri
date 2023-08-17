0c886ef67b273ba9590f488f3f0cd215fcc5a491
Jarno Luoma-Nirva <jln@iki.fi>
2023-08-17 23:09:26 +0300

    Session data to include transcripts and replies

--- api01/serializers.py
+++ api01/serializers.py
@@ -1,10 +1,25 @@
 import logging
 
 from rest_framework import serializers
-from .models import Session
+from .models import Session, Transcript, Reply
+
+
+class TranscriptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transcript
+        fields = ['session', 'audio_url', 'text', 'created']
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['session', 'audio_url', 'text', 'created']
 
 
 class SessionSerializer(serializers.ModelSerializer):
+    transcripts = TranscriptSerializer(many=True, read_only=True)
+    replies = ReplySerializer(many=True, read_only=True)
+
     class Meta:
         model = Session
-        fields = ['session_id', 'ip', 'agent', 'referer', 'created']
+        fields = ['session_id', 'ip', 'agent', 'referer', 'created', 'transcripts', 'replies']
--- api01/tests/test_views.py
+++ api01/tests/test_views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 from rest_framework import status
 
-from api01.models import Session
+from api01.models import Session, Transcript, Reply
 
 
 class StartSessionViewTests(TestCase):
@@ -30,11 +30,15 @@ class SessionListViewTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data_out = json.loads(response.content.decode())
         self.assertEqual(len(data_out), 0)
-        Session.objects.create(session_id="abc")
-        Session.objects.create(session_id="def")
+        session_1 = Session.objects.create(session_id="abc")
+        transcript_1 = Transcript.objects.create(session=session_1, text="trancript1")
+        reply_1 = Reply.objects.create(session=session_1, text="reply1")
         response = self.client.get(url)
         data_out = json.loads(response.content.decode())
-        self.assertEqual(len(data_out), 2)
+        self.assertEqual(len(data_out), 1)
+        self.assertEqual(data_out[0]["session_id"], "abc")
+        self.assertEqual(len(data_out[0]["transcripts"]), 1)
+        self.assertEqual(len(data_out[0]["replies"]), 1)
 
 
 class AboutViewTests(TestCase):
