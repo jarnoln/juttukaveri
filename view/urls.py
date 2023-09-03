7b5335b5f784001e0e3e2c6b9acaac86efa085e5
Jarno Luoma-Nirva <jln@iki.fi>
2023-09-03 17:02:35 +0300

    Add test workflow for GitHub

--- view/urls.py
+++ view/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("sessiot/", views.sessions, name="sessions"),
     path("j/", views.juttukaveri, name="juttukaveri"),
     path("", views.index, name="index"),
 ]
